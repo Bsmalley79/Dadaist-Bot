16005282cd479757867a8c37c85728cd1f73a1a4
Bsmalley79 <a1bookkeeping@gmail.com>
2015-09-07 18:05:05 -0400

    Integrated chain and tweetbot
    Moved all the twitter logic out of chain.py, now it just makes markov
    chains.  Tweet publishing handled by tweetbot.py

--- chain.py
+++ chain.py
@@ -4,45 +4,22 @@ import csv
 
 import tweepy
 
-nonword = "\n"  
+nonword = "\a"
 maxchar = 140  # Tweepy ignores strings over 140 characters
-testing = True
 
 
-def login():
-    try:
-        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-        auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-        api = tweepy.API(auth)
-        return api
-    except tweepy.error.TweepError:
-        print("Unable to contact Twitter services.
-              Please check your Internet connection.")
-        raise
-
-# Twitter validation information:
-target = []
-with open('keys.csv') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        target.append(row)
-CONSUMER_KEY = ''.join(target[0])
-CONSUMER_SECRET = ''.join(target[1])
-ACCESS_KEY = ''.join(target[2])
-ACCESS_SECRET = ''.join(target[3])
-
-dada = login()
-
-while testing:
+def chain(api):
     # GENERATE TABLE
     c1 = nonword
     c2 = nonword
     table = {}
-    statuses = dada.home_timeline(count=50)
+    statuses = api.home_timeline(count=50)
     for tweet in statuses:
         for char in tweet.text:
-            table.setdefault((c1, c2), []).append(char)
-            c1, c2 = c2, char
+            if not (c1 == c2 and c2 == char):
+                table.setdefault((c1, c2), []).append(char)
+                c1, c2 = c2, char
+    table.setdefault((c1, c2), []).append(nonword)  # mark EOF
 
 # GENERATE OUTPUT
     c1 = nonword
@@ -51,22 +28,8 @@ while testing:
 
     for i in range(maxchar):
         newchar = random.choice(table[(c1, c2)])
+        if newchar = nonword:
+            break
         twit += newchar
         c1, c2 = c2, newchar
-
-# PUBLISH TWEET
-    try:
-        dada.update_status(status=twit)
-    except tweepy.error.TweepError:
-        dada = login()
-        dada.update_status(status=twit)
-
-    try:
-        print(twit)
-    except UnicodeEncodeError:
-        print("The bot tried to speak unicode.
-              Check Tweet Deck for actual tweet.")
-    timer = random.randint(5, 60)  # at least 1/hr, no more than every 5 min
-    print(timer)
-    # time.sleep(timer * 60)
-    testing = False
+    return twit
--- tweetbot.py
+++ tweetbot.py
@@ -7,6 +7,19 @@ import csv
 import tweepy
 
 import dada
+import chain
+# import wordchain
+
+
+def login():
+    try:
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+        api = tweepy.API(auth)
+        return api
+    except tweepy.error.TweepError:
+        print("Unable to contact Twitter services.")
+        raise
 
 # Twitter validation information:
 target = []
@@ -14,27 +27,38 @@ with open('keys.csv') as f:
     reader = csv.reader(f)
     for row in reader:
         target.append(row)
-        
+
 CONSUMER_KEY = ''.join(target[0])
 CONSUMER_SECRET = ''.join(target[1])
 ACCESS_KEY = ''.join(target[2])
 ACCESS_SECRET = ''.join(target[3])
 
 try:
+    tbot = login()
     while True:
-        twit = dada.dada(0)
-# dada.dada takes a while to run. it's here to avoid twitter time out
-        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-        auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-        api = tweepy.API(auth)
-        api.update_status(status=twit)
-        print(twit)
-    
-        timer = random.randint(5, 60) #at least 1/hr but no more than every 5 minutes
+        roll = random.randint(1, 20)
+        if roll == 1:
+            twit = dada.dada(0)
+        # elif roll == 20: twit = wordchain.chain(tbot)
+        else:
+            try:
+                twit = chain.chain(tbot)
+            except tweepy.error.TweepError:
+                tbot = login()
+                twit = chain.chain(tbot)
+        tbot.update_status(status=twit)
+
+        try:
+            print(twit)
+        except UnicodeEncodeError:
+            print("The bot tried to speak unicode.")
+
+# At least 1 tweet every hour, but no more than every 5 minutes
+        timer = random.randint(5, 60)
         print(timer)
-        time.sleep(timer * 60) 
+        time.sleep(timer * 60)
+
 except KeyboardInterrupt:
     print("Thank you for using Dadaist Bot. Shutting down.")
 except tweepy.error.TweepError:
-    print("Unable to contact Twitter services.  Please check your Internet connection.")
-
+    pass
